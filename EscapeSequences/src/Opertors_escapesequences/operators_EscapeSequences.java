4d9f7fc73b58ef8e928321756b3ff030c759e6cc
chandan26 <40421920+PycoderCP@users.noreply.github.com>
2018-08-28 10:36:45 +0530

    Escape Sequence charecters in  java
    Detailed explantion of the escape charecters

---
+++ EscapeSequences/src/Opertors_escapesequences/operators_EscapeSequences.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Opertors_escapesequences;
+
+/**
+ *
+ * @author Administrator
+ */
+public class operators_EscapeSequences {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        
+        //  For next line "\n"
+        
+        // carriage return "\r"
+         
+        // form feed "\f"
+        
+        // for tab "\t"
+        // for  back  slash "\\"
+        System.out.println("\\");
+        System.out.println("my name is \t" + " chandan");
+        System.out.println("  What is your name \f"+"   shape Of you");
+         
+        // Logical Opertaors that can be useful in implementaion of complex logic 
+        // Main logical operator use in the implmentation are    “&”, “|”, “^”, and “~” or “!”
+        // || (OR) opertor 
+        
+        
+        
+         
+        
+        
+    }
+    
+}
