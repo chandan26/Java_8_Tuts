2e3fd4c32ae32de408d6c71a9fb2485714d77e8a
chandan26 <40421920+PycoderCP@users.noreply.github.com>
2018-08-28 12:35:02 +0530

    ID Generating Application
    Random ID generating application

---
+++ IDgenerator/src/Nlass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+import java.lang.System.*;
+import java.awt.font.*;
+public class Nlass {
+    
+    
+}
